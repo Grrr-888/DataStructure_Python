7d8c6a48c5fa36aa4948528157e68d2e469fa9ce
Grrr-888 <653535714@qq.com>
2019-07-28 18:14:33 +0800

    upload List operation code

---
+++ List_Operation.py
@@ -0,0 +1,83 @@
+# encoding = utf8
+
+class Node:
+    val = None
+    pNext = None
+
+    def __init__(self, value):
+        self.val = value
+
+    def output(self):
+        print(self.val)
+
+
+
+class List:
+    head = None
+    tail = None
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def addNode(self, value):
+
+        node = Node(value)
+
+        if None == self.head:
+            self.head = node
+            self.tail = node
+
+        if None != self.tail:
+            self.tail.pNext = node
+
+        self.tail = node
+
+    def reverse(self, start, end):
+
+        if start > end:
+            start, end = end, start
+
+        dummy_node = Node(None)
+        dummy_node.pNext = self.head
+        prev_node = dummy_node
+
+        for i in range(start - 1):
+            prev_node = prev_node.pNext
+
+        head_new = prev_node
+        prev_node = prev_node.pNext
+        cur_node = prev_node.pNext
+
+        for i in range(end - start):
+            prev_node.pNext = cur_node.pNext
+            cur_node.pNext = head_new.pNext
+            head_new.pNext = cur_node
+            cur_node = prev_node.pNext
+
+    def output(self):
+
+        node = self.head
+        while not None == node:
+            print(node.val)
+            node = node.pNext
+
+
+if __name__ == '__main__':
+    # node = Node(1)
+    #
+    # print(node.val)
+
+    lst = List()
+
+    lst.addNode(1)
+    lst.addNode(2)
+    lst.addNode(3)
+    lst.addNode(4)
+    lst.addNode(5)
+
+    lst.output()
+
+    lst.reverse(2, 3)
+
+    lst.output()
